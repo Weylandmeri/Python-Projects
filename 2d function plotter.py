f4b04fa788ca78de98cd2e833d7a9b2056126d27
Weylandmeri <96365719+Weylandmeri@users.noreply.github.com>
2022-01-03 16:45:14 -0800

    Update 2d function plotter.py

--- 2d function plotter.py
+++ 2d function plotter.py
@@ -21,7 +21,7 @@ for x in X:
 		try: # where the magic happens!   
 			#z = (1/(2*math.pi*sigma**2))*math.e**(-(x**2+y**2)/(2*sigma**2)) # normal distribution
 			#z = math.sqrt(-x**2-y**2+200**2) # Sphere
-			#z = math.sqrt(200*math.sqrt(x**2+y**2)-x**2-y**2-7500) # Torus
+			z = math.sqrt(200*math.sqrt(x**2+y**2)-x**2-y**2-7500) # Torus
 			#z = -abs(x*math.cos(math.pi/4)-y*math.sin(math.pi/4)) + -abs(x*math.sin(math.pi/4)+y*math.cos(math.pi/4)) # pyramid
 			#z = (1-x/200)**2 + 100*(y/200-(x/200)**2)**2 #Rosenbrock function
 			#z = (1.5-x+x*y)**2  + (2.25 - x + x*y**2)**2 + (2.625 -x + x*y**3)**2 #Beale function
@@ -33,14 +33,14 @@ for x in X:
 			#z = -math.sqrt(x**2 + y**2 - 20000) - math.sqrt(-x**2-y**2+200**2) # interesting pattern
 			#z = math.sqrt(x**2 + y**2) # cone
 			#z = x*y/(x**2 + y**2) # anisotrpic pattern
-			z  = x/y/x/y
+			#z  = x/y/x/y
 		except (ZeroDivisionError, ValueError, OverflowError):
 			z = 1
 		color.append(z)
 
 color = NormalizeData(color)
-color = [x*255 for x in color]
-colors = [[x, x, x] for x in color]
+#color = [x*255 for x in color]
+#colors = [[x, x, x] for x in color]
 
 
 def lerp_color(colors, value):
@@ -48,14 +48,14 @@ def lerp_color(colors, value):
 	color1 = pygame.Color(colors[int(index) % len(colors)])
 	color2 = pygame.Color(colors[int(index + 1) % len(colors)])
 	return color1.lerp(color2, fract)
-'''
+
 colors = []
 #colors = ["blue", "green", "yellow", "red"]
 colors = [(161,38,34), (255,248,196)]
 for i in color:
 	current_color = lerp_color(colors, i)
 	colors.append(current_color)
-'''
+
 index = 0
 for x in X:
 	for y in Y:
